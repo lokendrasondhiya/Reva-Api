ea4f41b7827b8c7d7c09657eeac317c03ef0bf14
lchikkala28 <lokeshchikkala.cas02@outlook.com>
2024-08-11 18:45:16 +0530

    Inital Create

--- Reva-Api/Program.cs
+++ Reva-Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Azure;
 using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var keyVaultUrl = builder.Configuration.GetSection("KeyVault:KeyVaultURL");
+
+var client = new SecretClient(new Uri(keyVaultUrl.Value!.ToString()), new DefaultAzureCredential());
+KeyVaultSecret secret = client.GetSecret("StorageConnection");
+string connectionstring = secret.Value;
+
 builder.Services.AddAzureClients(azureclient =>
 {
-    azureclient.AddBlobServiceClient(builder.Configuration.GetConnectionString("AzureSaConn"));
+    azureclient.AddBlobServiceClient(connectionstring);
     azureclient.UseCredential(new DefaultAzureCredential());
 });
 
